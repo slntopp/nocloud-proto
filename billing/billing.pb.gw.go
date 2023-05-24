b8b0064167352c620472f43f6baf0398ba3ca0e6
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2023-05-24 15:27:02 +0300

    Add anonymously

--- billing/billing.pb.gw.go
+++ billing/billing.pb.gw.go
@@ -239,9 +239,20 @@ func local_request_BillingService_ListPlans_0(ctx context.Context, marshaler run
 
 }
 
+var (
+	filter_BillingService_ListPlansInstances_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_BillingService_ListPlansInstances_0(ctx context.Context, marshaler runtime.Marshaler, client BillingServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListPlansInstancesRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BillingService_ListPlansInstances_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.ListPlansInstances(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
@@ -251,6 +262,13 @@ func request_BillingService_ListPlansInstances_0(ctx context.Context, marshaler 
 func local_request_BillingService_ListPlansInstances_0(ctx context.Context, marshaler runtime.Marshaler, server BillingServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListPlansInstancesRequest
 	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_BillingService_ListPlansInstances_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := server.ListPlansInstances(ctx, &protoReq)
 	return msg, metadata, err
