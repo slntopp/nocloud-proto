37390286662e1922c1034fd1816cfcda5b245e55
coddmeistr <euseew.maxim2015@yandex.ru>
2025-02-26 21:56:53 +0300

    python package

---
+++ proto/python/nocloud/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nocloud',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='coddmeistr',
+    author_email='euseew.maxim2015@yandex.ru',
+    description='nocloud api',
+    long_description='nocloud api long',
+    long_description_content_type='text/markdown',
+    url='https://github.com/slntopp/nocloud-proto',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
